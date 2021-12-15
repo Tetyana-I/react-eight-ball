f9f9aefda3b23e6e57f56805a4bbab4b8568ff50
Tetyana <iltaev2010@gmail.com>
2021-12-14 21:19:00 -0600

    refactored components App and EightBall

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-
 import './App.css';
-import {choice} from './helpers';
 import answers from './answers'
 import EightBall from './EightBall';
-import React, { useState } from "react";
+
 
 function App() {
-  const getAnswer = () => {
-    let answer = choice(answers);
-    setMsg(answer.msg);
-    setColor(answer.color);
-  }
-  const [msg, setMsg] = useState('Think of a Question');
-  const [color, setColor] = useState('black');
-
   return (
-    <div className = "App" onClick = { getAnswer } >
-      <h1>Magic Eight Ball</h1>
-        <EightBall msg = { msg } color = { color } />
+    <div className = "App">
+      <EightBall answers = {answers} />
     </div>
   );
 }
--- src/EightBall.js
+++ src/EightBall.js
@@ -1,10 +1,32 @@
 import './EightBall.css';
+import {choice} from './helpers';
+import { useState } from "react";
 
-const EightBall = ({ msg, color }) => (
+const EightBall = ({ answers }) => {
+    function getAnswer() {
+        let answer = choice(answers);
+        setMsg(answer.msg);
+        setColor(answer.color);
+    }
     
-    <div className = "EightBall"  style = {{ backgroundColor: color }}>
-        <p className = "EightBall-msg">{ msg }</p>
-    </div> 
-)
+    function resetBall() {
+        setMsg('Think of a Question');
+        setColor('black');
+    }
+
+    const [msg, setMsg] = useState('Think of a Question');
+    const [color, setColor] = useState('black');
+
+    return (
+        <>
+            <h2>Magic Eight Ball</h2>
+            <div className = "EightBall" onClick = { getAnswer } style = {{ backgroundColor: color }}>
+            <p className = "EightBall-msg">{ msg }</p> 
+            </div> 
+            <button className = "EightBall-btn" onClick = { resetBall }>Reset</button>       
+        </>
+    );
+}
 
 export default EightBall;
+
